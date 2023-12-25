3ad7f59e52aa2252270d4cf14fa3ccbe28239116
n-kaiyrken <111049118+n-kaiyrken@users.noreply.github.com>
2023-12-25 23:20:37 +0600

    Lesson 7 commit, add firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -80,4 +81,8 @@ dependencies {
     //GSON
     implementation ("com.google.code.gson:gson:2.10.1")
 
+    //FIREBASE
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-database:20.3.0")
 }
